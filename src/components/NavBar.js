4ce43df7d7815e2511bede04497dfe31563d6ca4
skuretski <skuretski@gmail.com>
2017-06-29 16:24:59 -0500

    Fixed NavBar syntax error

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+class NavBar extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="container-fluid">
+                <nav className="navbar navbar-default">
+                    
+
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
